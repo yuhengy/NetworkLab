cc00bcd23561ee88ab1f0704c44c421d222f72e2
YuhengYang <yangyuheng17@mails.ucas.ac.cn>
2020-10-28 23:56:48 +0800

    ENV: fix the vagrant environment

--- bootalways.sh
+++
@@ -1,3 +0,0 @@
-
-sudo sh -c "echo \"Acquire::http::proxy \\\"http://192.168.3.2:7890/\\\";\" > /etc/apt/apt.conf.d/30proxy"
-#sudo sh -c "echo \"Acquire::http::proxy \\\"http://10.200.175.54:7890/\\\";\" > /etc/apt/apt.conf.d/30proxy"
